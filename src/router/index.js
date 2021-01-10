7f941329540b0307cfd4f859be625c69c2230c14
MH <1992dayat@gmail.com>
2021-01-10 14:32:19 +0700

    admin protected

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,11 @@ import Admin from '../views/Admin.vue'
 import overview from '../views/overview.vue'
 import products from '../views/products.vue'
 import orders from '../views/orders.vue'
+import {fb} from '../firebase'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -22,6 +23,7 @@ export default new Router({
       path: '/admin',
       name: 'admin',
       component: Admin,
+      meta: {requiresAuth: true},
       children: [{
         path: 'overview',
         name: 'Overview',
@@ -49,3 +51,19 @@ export default new Router({
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const currentUser = fb.auth().currentUser
+
+  if (requiresAuth && !currentUser) {
+    next('/')
+  } else if (requiresAuth && currentUser) {
+    next()
+  } else {
+    next()
+  }
+});
+
+export default router;
